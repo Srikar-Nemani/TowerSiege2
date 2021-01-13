5655c9be4d410aea676c5a57daaeacbccf29e981
Srikar <ssnemani@outlook.com>
2021-01-12 18:51:44 -0800

    added visibility

---
+++ box2.js
@@ -0,0 +1,40 @@
+class Box2{
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':1.0,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.x=x;
+        this.y=y;
+        this.width = width;
+        this.height = height;
+        this.Visibility = 255;
+        World.add(world, this.body);
+      }
+      display(){
+          if(this.body.speed < 3){
+            
+            var pos =this.body.position;     
+            rectMode(CENTER);
+            fill("orange");
+            strokeWeight(1);
+            stroke("black");
+            rect(pos.x, pos.y, this.width, this.height);
+            
+
+          }
+          else{
+            push();
+            World.remove(world,this.body);
+            
+            this.Visibility -= 5;
+            tint(255,this.Visibility);
+            rect(0, 0, this.width, this.height);
+            pop();    
+          }
+       
+        
+    }
+}
